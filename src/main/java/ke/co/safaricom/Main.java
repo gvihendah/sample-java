0889fbf04d13987da07bffde1e7befddf5b3c9a2
Glorine Vihendah <92040630+gvihendah@users.noreply.github.com>
2023-06-26 01:39:56 +0300

    new file

---
+++ src/main/java/ke/co/safaricom/Main.java
@@ -0,0 +1,21 @@
+package ke.co.safaricom;
+
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("What year were you born?");
+    int birthYear = scanner.nextInt();
+    System.out.println("You were alive during this years: ");
+    for (int year = birthYear; year < 2023; year++) {
+      if (year % 4 == 0) {
+        System.out.println(year);
+      }
+    }
+  }
+}
+for
+
+
